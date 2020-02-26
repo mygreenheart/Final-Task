f8fb7687dc50782466076546e11f313486422cd2
Aleksandr Okhotnycov <sashaohotni26@gmail.com>
2020-02-26 19:50:44 +0200

    26/02/2020(3)

--- js/catalog_function.js
+++ js/catalog_function.js
@@ -3,11 +3,13 @@ let catalog = window.catalog,
     conteiner1 = document.getElementById("conteiner_new_arrivals"),
     conteiner2 = document.getElementById("conteiner_show_more"),
     select1 = document.getElementById("fashion"),
-    select2 = document.getElementById("fashion"),
+    select2 = document.getElementById("product_type"),
     select3 = document.getElementById("color"),
     select4 = document.getElementById("brand"),
     select5 = document.getElementById("size"),
-    select6 = document.getElementById("price_range");
+    select6 = document.getElementById("price_range"),
+    a = document.getElementsByClassName("filter_a"),
+    p = document.getElementsByClassName("filter_p");
 
 function createDiv(conteiner, img, name, price) {
     let divBanner = document.createElement("div"),
@@ -55,7 +57,10 @@ function drawBanners(conteiner, from, to) {
 }
 
 function fillAllSelect(select1, select3, select5) {
+    let fashion = new Set()
+    let color = new Set()
 
+    let size = new Set()                    //To do uniq array elements
     for (const e of catalog) {
         fashion.add(e.fashion)
         for (let i = 0; i < e.colors.length; i++) {
@@ -78,23 +83,78 @@ function fillAllSelect(select1, select3, select5) {
         let li = document.createElement("li"),
             a = document.createElement("a");
         a.innerText = e;
+        a.name = e;
         li.appendChild(a);
         select3.appendChild(li);
     }
-    for (const e of fashion) {
+    for (const e of size) {
         let li = document.createElement("li"),
             a = document.createElement("a");
         a.innerText = e;
+        a.name = e;
         li.appendChild(a);
         select5.appendChild(li);
     }
 }
 
-select1.onclick = function(event){
-    let target = event.target;
-for()
 
+//Events(DO REFACTORING)
+select1.onclick = function (event) {
+    let target = event.target;
+    if (target.name != undefined) {
+        a[0].innerText = target.name;
+        a[0].style.color = "#d14b58"
+        p[0].style.opacity = "1";
+        //  target.style.color = "#d14b58";
+    }
 }
+select2.onclick = function (event) {
+    let target = event.target;
+    if (target.name != undefined) {
+
+        a[1].innerText = target.name;
+        a[1].style.color = "#d14b58"
+        p[1].style.opacity = "1";
+        // target.style.color = "#d14b58";
+    }
+}
+select3.onclick = function (event) {
+    let target = event.target;
+    if (target.name != undefined) {
+        a[2].innerText = target.name;
+        a[2].style.color = "#d14b58"
+        p[2].style.opacity = "1";
+        //  target.style.color = "#d14b58";
+    }
+}
+select4.onclick = function (event) {
+    let target = event.target;
+    if (target.name != undefined) {
+        a[3].innerText = target.name;
+        a[3].style.color = "#d14b58"
+        p[3].style.opacity = "1";
+        //target.style.color = "#d14b58";
+    }
+}
+select5.onclick = function (event) {
+    let target = event.target;
+    if (target.name != undefined) {
+        a[4].innerText = target.name;
+        a[4].style.color = "#d14b58"
+        p[4].style.opacity = "1";
+        //  target.style.color = "#d14b58";
+    }
+}
+select6.onclick = function (event) {
+    let target = event.target;
+    if (target.name != undefined) {
+        a[5].innerText = target.name;
+        a[5].style.color = "#d14b58"
+        p[5].style.opacity = "1";
+        // target.style.color = "#d14b58";
+    }
+}
+
 fillAllSelect(select1, select3, select5);
 drawBanners(conteiner1, 0, 4)
 drawBanners(conteiner2, 4, 12)
